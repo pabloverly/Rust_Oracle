28b7c9b31208742d0ecd5ea446b0d134fc3aeb61
pabloverly <pablopierre.pv@gmail.com>
2022-11-27 18:43:39 -0300

    27-11-22

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 [https://github.com/kubo/rust-oracle]
+
 [https://oracle.github.io/odpi/doc/installation.html]
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,20 @@ fn main() -> Result<()> {
     let username = "dbati";
     let password = "dbati";
     let database = "//localhost/XE";
-    let sql = "select * from temp";
+    let sql = "select * from dbati.temp";
 
     let conn = Connection::connect(username, password, database)?;
     let mut stmt = conn.statement(sql).build()?;
     let rows = stmt.query(&[])?;
 
     // print column types
-    for (idx, info) in rows.column_info().iter().enumerate() {
-        if idx != 0 {
-            print!(",");
-        }
-        print!("{}", info);
-    }
-    println!();
+    // for (idx, info) in rows.column_info().iter().enumerate() {
+    //     if idx != 0 {
+    //         print!(",");
+    //     }
+    //     print!("{}", info);
+    // }
+    // println!();
 
     for row_result in rows {
         // print column values
